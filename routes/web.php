c88896c3c4e3f8241ba626cc86583c8d1da644bb
Maysa Artawan <maysaartawan@sanatasystem.com>
2026-02-09 17:42:41 +0800

    fix: route dashboard links to modules

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect()->route('modules.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/dashboard-modules', [ModulesDashboardController::class, 'index'])
